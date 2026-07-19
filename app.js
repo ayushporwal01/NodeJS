6df27c4a48fa0948fa75ac6b61d3e22581ec2ce1
ayushporwal01 <porwalayush953@gmail.com>
2026-07-19 16:30:34 +0530

    synchronous code

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const { calculateSum, calculateMultiply } = require("./calculate");
 var a = 6;
 var b = 9;
 
+const data = require("./data.json");
+console.log(data);
+
 calculateSum(a, b);
 calculateMultiply(a, b);
---
+++ sync.js
@@ -0,0 +1,11 @@
+console.log("Hello i'm in sync");
+
+var a = 123456789;
+var b = 987654321;
+
+function multiplyFn() {
+  var result = a * b;
+  console.log(result)
+}
+
+var c = multiplyFn();
