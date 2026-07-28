6b76c98b76972dd906be30a0d30bba76df368666
ayushporwal01 <porwalayush953@gmail.com>
2026-07-28 12:17:29 +0530

    event loop-3

---
+++ event-loop-3.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+setImmediate(() => console.log("setImmediate"));
+
+Promise.resolve("Promise").then(console.log);
+
+setTimeout(() => {
+  console.log("Timer expired");
+}, 0);
+
+fs.readFile("./file.txt", "utf8", () => {
+  setTimeout(() => {
+    console.log("2nd timer");
+  }, 0);
+
+  process.nextTick(() => console.log("2nd nextTick"));
+
+  setImmediate(() => console.log("2nd setImmediate"));
+
+  console.log("File Reading CB");
+});
+
+process.nextTick(() => console.log("nextTick"));
+
+console.log("Last line of the file");
