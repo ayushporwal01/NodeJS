f70529cd88138149ad9d4d7e156fa9d859c48921
ayushporwal01 <porwalayush953@gmail.com>
2026-07-30 17:34:29 +0530

    thread-pool

--- thread-pool.js
+++ thread-pool.js
@@ -1,18 +1,19 @@
-process.env.UV_THREADPOOL_SIZE = 3;
 const crypto = require("crypto");
 
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+process.env.UV_THREADPOOL_SIZE = 5;
+
+crypto.pbkdf2("password", "salt", 10000000, 50, "sha512", (err, key) => {
   console.log("1 - cryptoPBKDF2 done");
 });
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+crypto.pbkdf2("password", "salt", 10000000, 50, "sha512", (err, key) => {
   console.log("2 - cryptoPBKDF2 done");
 });
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+crypto.pbkdf2("password", "salt", 10000000, 50, "sha512", (err, key) => {
   console.log("3 - cryptoPBKDF2 done");
 });
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+crypto.pbkdf2("password", "salt", 10000000, 50, "sha512", (err, key) => {
   console.log("4 - cryptoPBKDF2 done");
 });
-crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+crypto.pbkdf2("password", "salt", 10000000, 50, "sha512", (err, key) => {
   console.log("5 - cryptoPBKDF2 done");
 });
