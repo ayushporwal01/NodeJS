fdf123012f1ad011cb35da42f161eec802981262
ayushporwal01 <porwalayush953@gmail.com>
2026-07-31 11:22:14 +0530

    created a http server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
+  if (req.url === "/getSecretData") {
+    res.end("There is no secret data");
+  }
   res.end("Hello World!");
 });
 
